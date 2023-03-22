0d7e5311f6051838853766ea04e66a054d85b5a8
Matias Aramayo <matiasaramayo@hotmail.com>
2023-03-21 23:43:22 -0300

    .

--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,10 @@
 import {Router} from 'express'
-import { deletePost, getPosts, postPost, updatePost } from '../controllers/post.js'
+import { deletePost, getPost, getPosts, postPost, updatePost } from '../controllers/post.js'
 
 export const postRoutes = Router()
 
 postRoutes.get('/', getPosts )
+postRoutes.get('/', getPost)
 postRoutes.post ('/', postPost)
 postRoutes.put('/:id', updatePost)
 postRoutes.delete('/:id', deletePost)
